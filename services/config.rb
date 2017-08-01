57b79ea54eebbbfe7f64845c8fe6d99427617179
Nandesh Guru <nandesh@cloudcoreo.com>
2017-07-31 22:34:41 -0700

    Update config.rb

--- services/config.rb
+++ services/config.rb
@@ -21,41 +21,31 @@ coreo_agent_audit_rule 'echo-hello' do
   control 'echo-hello' do
     impact 1.0
     describe command('echo hello') do
-      its('stdout') { should eq "world\n" }
+      its('stdout') { should eq "hello\n" }
       its('stderr') { should eq '' }
       its('exit_status') { should eq 0 }
     end
   end
 end
 
-coreo_agent_selector_rule "check-mysql-installed" do
-  action :define
-  timeout 5
-  control 'check if echo exist' do
-    describe command('mysqld') do
-      it { should exist }
-    end
-  end
-end
-
-coreo_agent_audit_rule "mysql-env-password" do
+coreo_agent_audit_rule "env-user-password" do
   action :define
   link "http://kb.cloudcoreo.com/"
-  display_name "Do not store your MySQL password in your ENV"
+  display_name "Do not store your user password in your ENV"
   description "Storing credentials in your ENV may easily expose them to an attacker. Prevent this at all costs."
   category "Security"
-  suggested_action "Unset MySQL password in your ENV"
+  suggested_action "Unset User password in your ENV"
   level "High"
-  selectors ["check-mysql-installed"]
-  control 'mysql-3' do
+  selectors ['check-echo']
+  control 'user-password' do
     impact 1.0
-    title 'Do not store your MySQL password in your ENV'
+    title 'Do not store your User password in your ENV'
     desc '
   Storing credentials in your ENV may easily expose
   them to an attacker. Prevent this at all costs.
       '
     describe command('env') do
-      its('stdout') { should_not match(/^MYSQL_PWD=/) }
+      its('stdout') { should_not match(/^USER_PWD=/) }
     end
   end
   timeout 5
