db8ca29172f79d706cd53953fcd833a48f05b2f9
Nandesh Guru <nandesh@cloudcoreo.com>
2017-07-31 23:08:55 -0700

    Update config.rb

--- services/config.rb
+++ services/config.rb
@@ -28,6 +28,16 @@ coreo_agent_audit_rule 'echo-hello' do
   end
 end
 
+coreo_agent_selector_rule "check-redis-cli" do
+  action :define
+  timeout 5
+  control 'check if echo exist' do
+    describe command('redis-cli') do
+      it { should exist }
+    end
+  end
+end
+
 coreo_agent_audit_rule "env-user-password" do
   action :define
   link "http://kb.cloudcoreo.com/"
@@ -36,7 +46,7 @@ coreo_agent_audit_rule "env-user-password" do
   category "Security"
   suggested_action "Unset User password in your ENV"
   level "High"
-  selectors ['check-echo']
+  selectors ['check-redis-cli']
   control 'user-password' do
     describe command('env') do
       its('stdout') { should_not match(/^USER_PWD=/) }
